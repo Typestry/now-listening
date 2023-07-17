96ab723b3b6a69c639350385ce76845e6807c819
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2023-07-17 11:13:35 -0500

    added inquirer and fileWrite

--- index.js
+++ index.js
@@ -1,45 +1,58 @@
-const applescript = require("applescript");
-const axios = require("axios");
-const nodeChron = require("node-cron")
-require("dotenv").config()
+import inquirer from "inquirer";
+import { startStatusTask } from "./startStatusTask.js";
+import { readFileSync, writeFile } from "fs";
 
-const options = {
-  method: "POST",
-  url: "https://slack.com/api/users.profile.set",
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
-  },
-};
+(async () => {
+  try {
+    const response = readFileSync("config.txt", "utf8");
+    const { token, provider } = JSON.parse(response);
+    console.log("Successfully read config file! Starting task! 🎉");
+    startStatusTask(provider, token);
+  } catch (err) {
+    const {
+      token,
+      options: [provider],
+    } = await getAnswers();
+    const content = JSON.stringify({ token, provider });
+    writeFile("config.txt", content, (err) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log("Successfully wrote config file! 🎉");
+    });
+    startStatusTask(provider, token);
+  }
+})();
 
-const script = `tell application "${process.env.MUSIC_PROVIDER}" to get player state & (get {name, artist} of current track)`
-
-const updateStatus = () => {
-    applescript.execString(
-        script,
-        async (err, result) => {
-            if (err) {
-                return
-            }
-
-            const [state, song, artist] = await result;
-            
-            if (state !== "paused") {
-                const status_emoji = '🎶';
-                const status_text = `${song} by ${artist}`;
-                const data = { profile: { status_emoji, status_text } };
-
-                await axios
-                    .request({ ...options, data })
-                    .then(function () {
-                        console.log(`Successfully updated status with: ${status_emoji} ${status_text}`);
-                    })
-                    .catch(function (error) {
-                        console.error(error);
-                    });
-            }
+function getAnswers() {
+  return inquirer.prompt([
+    {
+      name: "token",
+      message: "What is your slack app token?",
+      type: "input",
+      validate: (token) => {
+        if (!token.length) {
+          return "Please provide a token";
         }
-    );
-}
+        return true;
+      },
+    },
+    {
+      name: "options",
+      message: "Who is your music provider?",
+      type: "checkbox",
+      choices: ["Music", "Spotify"],
+      validate: (options) => {
+        if (!options.length) {
+          return "Choose at least one of the above, use space to choose the option";
+        }
 
-nodeChron.schedule("* * * * *", updateStatus)
+        if (options.length > 1) {
+          return "Please select only one option";
+        }
+
+        return true;
+      },
+    },
+  ]);
+}
---
+++ startStatusTask.js
@@ -0,0 +1,45 @@
+import applescript from "applescript"
+import axios from "axios"
+import nodeCron from "node-cron"
+import { config } from "dotenv"
+
+config()
+
+export const startStatusTask = (provider, token) => {
+  const options = {
+    method: "POST",
+    url: "https://slack.com/api/users.profile.set",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const script = `tell application "${provider}" to get player state & (get {name, artist} of current track)`;
+  const task = () => {
+    applescript.execString(script, async (err, result) => {
+      if (err) {
+        return;
+      }
+
+      const [state, song, artist] = await result;
+
+      if (state !== "paused") {
+        const status_emoji = "🎶";
+        const status_text = `${song} by ${artist}`;
+        const data = { profile: { status_emoji, status_text } };
+
+        await axios
+          .request({ ...options, data })
+          .then(function () {
+            console.log(
+              `Successfully updated status with: ${status_emoji} ${status_text}`
+            );
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      }
+    });
+  };
+  nodeCron.schedule("* * * * *", task);
+};

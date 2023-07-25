ff94093aca5e7f9cecbcb761efa3b15cfdea4e49
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2023-07-25 15:04:07 -0500

    removed nodeCron added breaks

--- src/utils/statusTask.ts
+++ src/utils/statusTask.ts
@@ -1,20 +1,20 @@
-import nodeCron from "node-cron"
 import { getStatusMac } from "./getStatusMac"
 import { updateStatus } from "../api/status/updateStatus"
 import { MusicProvider } from "../types/MusicProvider"
 
-export const statusTask = (provider: MusicProvider) => {
-  const task = async () => {
-    switch (process.platform) {
-      case "darwin":
-        await getStatusMac(provider).then(updateStatus)
-      case "linux":
-        throw Error("Linux is not currently supported.")
-      case "win32":
-        console.error("Windows is not currently supported.")
-      default:
-        throw Error(`Operating system is not supported.`)
-    }
+export const statusTask = async (provider: MusicProvider) => {
+  switch (process.platform) {
+    case "darwin":
+      const payload = await getStatusMac(provider)
+      await updateStatus(payload)
+      break
+    case "linux":
+      console.error("Linux is not currently supported.")
+      break
+    case "win32":
+      console.error("Windows is not currently supported.")
+      break
+    default:
+      console.error(`Operating system is not supported.`)
   }
-  nodeCron.schedule("* * * * *", task)
 }

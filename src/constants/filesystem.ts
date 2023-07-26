2210fdcd6e7f4e0e2fe36bf24ff42cfee3824465
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2023-07-26 10:17:16 -0500

    fixed references to directory

--- src/constants/filesystem.ts
+++ src/constants/filesystem.ts
@@ -1,3 +1,9 @@
+import path from "path"
+import { fileURLToPath } from "url"
+
+const filename = fileURLToPath(import.meta.url)
+const SRC = path.dirname(filename).split("/constants")[0]
+
 export const FILE_NAME = "config.txt"
-export const FILE_PATH = __dirname
+export const FILE_PATH = `${SRC}/${FILE_NAME}`
 export const ENCODING = "utf8"

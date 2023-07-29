f08295cbb7a4256486542a57a6b66832ef2cdce9
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2023-07-28 22:38:20 -0500

    added eslint and other niceties

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import typescript from "@rollup/plugin-typescript"
 import json from "@rollup/plugin-json"
+import run from "@rollup/plugin-run"
 
 export default {
   input: "src/app.ts",
   output: {
     dir: "dist",
     format: "es",
+    sourcemap: true,
   },
-  plugins: [typescript(), json()],
+  plugins: [
+    typescript(),
+    json(),
+    run({
+      execArgv: ["-r", "source-map-support/register"],
+    }),
+  ],
 }

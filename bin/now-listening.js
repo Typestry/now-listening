5f6722d9788ef7b3ce70cf31e14810936bd4636f
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2023-08-02 17:43:03 -0500

    added config command (#59)

--- bin/now-listening.js
+++ bin/now-listening.js
@@ -1,4 +1,19 @@
 #! /usr/bin/env node
+import fs from "fs"
+import yargs from "yargs"
 import { app } from "../dist/app.js"
+
+yargs(process.argv.slice(2)).command(
+  "config",
+  "Overwrite current config",
+  (yargs) => {
+    yargs.positional("config", { default: true, type: "boolean" })
+  },
+  (args) => {
+    if (args.config) {
+      fs.writeFileSync("./dist/config.txt", "")
+    }
+  },
+).argv
 
 app()
